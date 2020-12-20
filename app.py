ee3378c9dde3eb733016a6bbd877e4cafc8721a5
Young proger <66271043+Yourun-proger@users.noreply.github.com>
2020-12-20 19:36:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import data
 from flask import Flask, render_template
+import data
 
 app = Flask(__name__)
 
@@ -40,6 +40,18 @@ def departures(departure):
 @app.route("/tours/<int:id>/")
 def tours(id):
     return render_template("tour.html", tour=data.tours[id], id=id, departures=data.departures)
+
+
+@app.errorhandler(500)
+def render_server_error(error):
+    page = "<h1>500 error</h1><br><h2>Из-за некорректого ввода произошла ошибка на сервере...</h2>"
+    return page
+
+
+@app.errorhandler(404)
+def render_not_found(error):
+    page = "<h1>404 error</h1><br><h2>Сайт не нашёл тура или направления...</h2>"
+    return page
 
 
 if __name__ == '__main__':
